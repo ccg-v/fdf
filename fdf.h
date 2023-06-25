6749474eccb37eec97fb963df7efada8de526a10
Carlos Carracedo garcia-villalba <ccarrace@car9s5.42barcelona.com>
2023-06-26 00:15:32 +0200

    ...

--- fdf.h
+++ fdf.h
@@ -6,22 +6,26 @@
 /*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 19:15:57 by ccarrace          #+#    #+#             */
-/*   Updated: 2023/06/23 18:31:07 by ccarrace         ###   ########.fr       */
+/*   Updated: 2023/06/25 20:51:43 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# include <stdlib.h>
+# include <stdlib.h>    // free, size_t
+# include <stdio.h>     // printf
 
-typedef struct t_map
+typedef struct s_map
 {
     int     width;
     int     length;
     int     **height;
     void    *mlx_ptr;
     void    *win_ptr;
-}           map;
+}           t_map;
+
+char    **ft_split(char const *s, char c);
+int	    ft_atoi(const char *str);
 
 #endif
--- ft_read_file.c
+++ ft_read_file.c
@@ -6,13 +6,13 @@
 /*   By: ccarrace <ccarrace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 19:32:01 by ccarrace          #+#    #+#             */
-/*   Updated: 2023/06/24 01:59:44 by ccarrace         ###   ########.fr       */
+/*   Updated: 2023/06/25 23:48:59 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
     gcc -g -Wall -Wextra -Werror -I gnl/ ft_read_file.c get_next_line/get_next_line.c 
-    get_next_line/get_next_line_utils.c
+    get_next_line/get_next_line_utils.c ft_split.c ft_atoi.c
 */
 
 #include "fdf.h"
@@ -39,7 +39,6 @@ static int	count_elements(const char *str, char c)
 	return (num_elements);
 }
 
-
 static int get_map_length(char *file_name)
 {
     int     fd;
@@ -53,11 +52,37 @@ static int get_map_length(char *file_name)
     {
         line = get_next_line(fd);
         map_length++;
+        free(line);
     }
     close(fd);
     return (map_length);
 }
 
+static int get_map_width(char *file_name)
+{
+    char    *line;
+    int     fd;
+    int     map_width;
+    int     flag;
+    
+    fd = open(file_name, O_RDONLY, 0);
+    line = "";
+    flag = 0;
+    while(line != NULL)
+    {
+        line = get_next_line(fd);
+        if (flag == 0)
+        {
+            map_width = count_elements(line, ' ');
+            flag = 1;
+        }
+        free(line);
+    }
+    close(fd);
+    return (map_width);
+}
+
+/*
 static int get_map_width(char *file_name)
 {
     char    *line;
@@ -71,59 +96,74 @@ static int get_map_width(char *file_name)
     close(fd);
     return (map_width);
 }
+*/
 
-int main(int argc, char **argv)
-{    
-//    struct t_map   map;
-
-    int map_width;
-    int map_length;
-
-    if (argc == 1)
-        write (1, "No file specified!\n", 19);
-    else if (argc > 2)
-        write (1, "Too may arguments!\n", 20);
-    else
-    {
-        map_length = get_map_length(argv[1]);
-        map_width = get_map_width(argv[1]);
-        printf("Width of map is %d\n", map_width);
-        printf("Length of map is %d\n", map_length);
-    }
-    return (0);
-}
-
-void    ft_read_file(char *file_name, struct t_map map)
+void    read_file(char *file_name, t_map *map)
 {
     int     fd;
     char    *line;
+    char    **splitted_line;
     int     i;
     int     j;
 
-    map.length = get_map_length(file_name);
-    map.width = get_map_width(file_name);
-    map.height = malloc((map.length + 1) * sizeof(int *));
+    map->length = get_map_length(file_name);
+    map->width = get_map_width(file_name);
+    map->height = malloc((map->length + 1) * sizeof(int *));
     i = 0;
-    while (i <= map.length)
+    while (i < map->length)
     {
-        map.height[i] = malloc((map.width + 1) * sizeof(int));
+        map->height[i] = malloc((map->width + 1) * sizeof(int));
         i++;
     }
     fd = open(file_name, O_RDONLY, 0);
     i = 0;
-    while (i <= map.length)
+    line = NULL;
+    while (i < map->length )
     {
+       line = get_next_line(fd);
+       splitted_line = ft_split(line, ' ');
+       free(line);
+//        splitted_line = ft_split(get_next_line(fd), ' ');
         j = 0;
-        while (j <= map.width)
+        while (j < map->width)
         {
-            map.height[i][j] = 
+            map->height[i][j] = ft_atoi(splitted_line[j]);
             j++;
+            free(splitted_line[j]);
         }
+        free(splitted_line);
         i++;
     }
+    close(fd);
+    
+}
 
+int main(int argc, char **argv)
+{
+    t_map   *map;
+    int     i;
+    int     j;
 
-
+    (void)argc;
+    map = (t_map *)malloc(sizeof(t_map));
+    if (map == NULL)
+        return (1);
+    read_file(argv[1], &(*map));
+    i = 0;
+printf("map->length = %d\n", map->length);
+printf("map->width  = %d\n", map->width);
+    while (i < map->length)
+    {
+        j = 0;
+        while (j < map->width)
+        {
+            printf("%2d ", map->height[i][j]);
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    free(map);
 }
 
 /*
